21df27aed6323d5221eb143c6518baec817f14ec
taylorteep2 <taylorteep2@yahoo.com>
2021-03-09 15:14:51 -0600

    Initial commit

---
+++ src/main/java/com/tts/TransitApp/controller/TransitController.java
@@ -0,0 +1,38 @@
+package com.tts.TransitApp.controller;
+
+import com.tts.TransitApp.model.Bus;
+import com.tts.TransitApp.model.BusRequest;
+import com.tts.TransitApp.service.TransitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class TransitController {
+
+    @Autowired
+    private TransitService apiService;
+
+    public TransitController(TransitService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping("/")
+    public String getBusesPage(Model model){
+        model.addAttribute("request", new BusRequest());
+        return "index";
+    }
+
+    @PostMapping("/buses")
+    public String getNearbyBuses(BusRequest request, Model model) {
+        List<Bus> buses = apiService.getNearbyBuses(request);
+        model.addAttribute("buses", buses);
+        model.addAttribute("request", request);
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/tts/TransitApp/model/DistanceResponse.java
@@ -0,0 +1,11 @@
+package com.tts.TransitApp.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DistanceResponse {
+    public List<Row> rows;
+
+}
